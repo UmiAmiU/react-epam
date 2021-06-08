2b4fe6b070023766c2f518d50233b0f43b8fb838
Youtop1a <oleeof@gmail.com>
2021-06-08 12:51:01 +0300

    Implement form validation

--- src/components/CardCreationForm/CardCreationForm.jsx
+++ src/components/CardCreationForm/CardCreationForm.jsx
@@ -1,14 +1,36 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './CardCreationForm.module.scss';
 
 function CardCreationForm(props) {
   const { handler } = props;
   const header = useRef(null);
   const text = useRef(null);
+  const [error, setError] = useState({});
 
   const clearValue = () => {
     header.current.value = '';
     text.current.value = '';
+  };
+
+  const checkValue = (event) => {
+    let errorObj = {};
+    if (header.current.value === '') {
+      errorObj['header'] = true;
+      header.current.scrollIntoView({ block: 'center', behavior: 'smooth' });
+    } else {
+      errorObj['header'] = false;
+    }
+    if (text.current.value === '') {
+      errorObj['text'] = true;
+      text.current.scrollIntoView({ block: 'center', behavior: 'smooth' });
+    } else {
+      errorObj['text'] = false;
+    }
+    if (!errorObj.header && !errorObj.text) {
+      handler(event);
+      clearValue();
+    }
+    setError({ ...errorObj });
   };
 
   return (
@@ -24,6 +46,9 @@ function CardCreationForm(props) {
           ref={header}
         />
       </div>
+      {error.header && (
+        <div className={styles['error']}>this field is required</div>
+      )}
       <div className={styles['info']}>Text input: </div>
       <div className={styles['input-block']}>
         <textarea
@@ -34,13 +59,13 @@ function CardCreationForm(props) {
           ref={text}
         ></textarea>
       </div>
+      {error.text && (
+        <div className={styles['error']}>this field is required</div>
+      )}
       <input
         type="submit"
         value="Submit"
-        onClick={(event) => {
-          handler(event);
-          clearValue();
-        }}
+        onClick={(event) => checkValue(event)}
         className={styles.submit}
       />
     </div>
